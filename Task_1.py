4e8f939694ce501edd1e609af39d3f0c9bc22a29
chaitanyarana8 <chaitanyarana.mnd@gmail.com>
2025-10-07 03:26:43 +0530

    Uploaded latest Python assignments

---
+++ Task_1.py
@@ -0,0 +1,5 @@
+user_input = int(input("Enter a Number: "))
+if user_input%2 == 0:
+    print(f"{user_input} is even number.")
+else:
+    print(f"{user_input} is odd number.")
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+count = 0
+for i in range(1,51):
+    count += i
+print("final sum is ",count)
