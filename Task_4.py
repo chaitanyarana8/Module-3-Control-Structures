f0dafdf86fb9beba5f4c879435db82e2f0331ddb
chaitanyarana8 <chaitanyarana.mnd@gmail.com>
2025-10-08 08:49:34 +0530

    using different method

--- Task_4.py
+++ Task_4.py
@@ -2,3 +2,5 @@ count = 0
 for i in range(1,51):
     count += i
 print(f"The sum of first 50 natural numbers is: {count}")
+# range(51) means 0 to 50 you neeed to start form 1 to 50 you can use like this range(1,51)or range(1,50+1) 
+# you can also use math fromula n(n+1)//2
